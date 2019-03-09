9590edd521a57d2e6d1ad67ac7ea498badb8b4bf
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-03-09 16:18:50 +0530

    update

--- src/app/checkout/cart/cart.page.ts
+++ src/app/checkout/cart/cart.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../provider/cart/cart.service';
+import { CartService } from '../../provider/cart/cart.service';
 import { ActivatedRoute } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 @Component({
